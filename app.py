afbcb642d94489711fe2d93539af815700b9f47f
Natalie Clark <nj_clark@hotmail.co.uk>
2023-12-21 20:05:48 +0000

    Delete redundant files and rename design recipe

--- app.py
+++ app.py
@@ -7,12 +7,6 @@ connection = DatabaseConnection()
 connection.connect()
 
 # Seed with some seed data
-connection.seed("seeds/music_library.sql")
+connection.seed("seeds/student_directory_2.sql")
 
-# Retrieve all artists
-artist_repository = ArtistRepository(connection)
-artists = artist_repository.all()
-
-# List them out
-for artist in artists:
-    print(artist)
+# Add code here...
